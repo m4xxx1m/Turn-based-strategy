aeee4e16ad99f6853343d992c5e8b011f16fc680
eyakm1 <eyakm1@protonmail.com>
2023-06-18 01:00:23 +0300

    Selected troopers savegame class implemented

--- Source/TurnBasedTutorial/BattleMode/BattleUI.cpp
+++ Source/TurnBasedTutorial/BattleMode/BattleUI.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "BattleUI.h"
 #include "Components/Button.h"
 #include "Components/TextBlock.h"
-#include "BattleUI.h"
 #include "BattlePlayerController.h"
 #include "BattlePlayerState.h"
 
---
+++ Source/TurnBasedTutorial/ManageSquad/SelectedTrooperSaveGame.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SelectedTrooperSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEDTUTORIAL_API USelectedTrooperSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category=Basic)
+	TArray<uint8> SelectedTroopers;
+
+	USelectedTrooperSaveGame();
+};
